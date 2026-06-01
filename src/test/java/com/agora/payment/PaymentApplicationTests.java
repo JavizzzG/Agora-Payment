6f5a64df0e6e967c2a23a6bcbb3b4c2327ba05bd
JavizzzG <javierstivengalvis2008@gmail.com>
2026-06-01 06:25:37 -0500

    initial structure

---
+++ src/test/java/com/agora/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agora.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
